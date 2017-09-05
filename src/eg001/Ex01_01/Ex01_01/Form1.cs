312b6ca2ab734c2dbc8c6e82202cdb6d403b92bf
chengwengao <657325704@qq.com>
2017-09-05 17:43:52 +0800

    实例001，有bug，仍需调试

---
+++ src/eg001/Ex01_01/Ex01_01/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex01_01
+{
+    public partial class Form1 : Form
+    {
+        private string address = "c:\\";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 关闭所有ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            this.openFileDialog1.ShowDialog();
+            StreamWriter s = new StreamWriter(address + "\\Menu.ini", true);
+            s.WriteLine(openFileDialog1.FileName);//写入 INI 文件
+            s.Flush();
+            s.Close();
+            ShowWindows(openFileDialog1.FileName);
+        }
+
+        //自定义方法ShowWindows()用来加载北京图片并显示窗体
+        public void ShowWindows(string fileName)
+        {
+            Image p = Image.FromFile(fileName);
+            Form f = new Form();
+            f.MdiParent = this;
+            f.BackgroundImage = p;
+            f.Show();
+        }
+
+        //读取INI文件并将信息加入菜单
+        private void Form1_Load()
+        {
+            StreamReader sr = new StreamReader(address + "\\Menu.ini");
+            int i = this.文件ToolStripMenuItem.DropDownItems.Count - 2;
+            //读取INI文件
+            while (sr.Peek() >= 0)
+            {
+                ToolStripMenuItem menuItem = new ToolStripMenuItem(sr.ReadLine());
+                this.文件ToolStripMenuItem.DropDownItems.Insert(i, menuItem);
+                i++;
+                menuItem.Click += new EventHandler(menuItem_Click);
+            }
+            sr.Close();
+        }
+
+        private void menuItem_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
